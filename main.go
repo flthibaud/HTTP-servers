c97fbdf7015152602a05e80332a9409a082b7e6a
fthibaud <thibaud@wacan.com>
2025-05-10 11:03:50 +0200

    Implement metrics middleware for file server and add /metrics and /reset endpoints

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
+		a.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+
+	apiCfg := &apiConfig{}
+
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
+	handler = apiCfg.middlewareMetricsInc(handler)
+
+	mux.Handle("/app/", handler)
+
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d\n", apiCfg.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
 	})
 
 	srv := &http.Server{
